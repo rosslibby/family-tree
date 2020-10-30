89b4cd4aea18dfc0f32049f10fcad28f7cb55637
Ross Libby <ross@frvctl.com>
2020-10-29 22:02:23 -0500

    added profile editing link

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Page = ({children, centered}) => (
   <div>
     <div className="header">
       <h1>Family Tree</h1>
+      <Link to="/settings">Profile</Link>
     </div>
     <div className="content">
       <div className={`page${centered ? ' page--centered' : ''}`}>{children}</div>
