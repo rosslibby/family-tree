75015b9dab4fdef8ae31b04f9b539eebf8bff747
Ross Libby <ross@frvctl.com>
2020-10-29 20:58:22 -0500

    added people data to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,10 @@ import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
 import reducer from 'reducers'
 
+import people from 'store/people'
+
 const store = createStore(reducer, {
+  people
 }, applyMiddleware(reduxThunk))
 
 export default store
