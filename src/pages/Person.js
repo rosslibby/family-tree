6e6c355c3f1e3f2f7e745fa7c0dec1b0d8733313
Ross Libby <ross@frvctl.com>
2020-10-29 22:01:27 -0500

    people switching is a go

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Card from 'components/Card/Card'
 import Page from 'components/Page/Page'
-import { withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class Person extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {person: props.people.person, personId: props.match.params.id}
+    this.state = {person: props.people.person || null, personId: props.match.params.id}
   }
 
   static getDerivedStateFromProps(props, state) {
-    if (props.match.params.id == state.personId) return {
+    console.log(props.match.params.id != state.personId)
+    if (props.match.params.id != state.personId) return {
       person: props.people.list.filter(person => person.id == props.match.params.id)[0],
       personId: props.match.params.id
+    }
+    else if (props.match.params.id === state.personId && !state.person) return {
+      person: props.people.list.filter(person => person.id == props.match.params.id)[0]
     }
     return null
   }
@@ -24,7 +28,7 @@ class Person extends React.Component {
 
   render() {
     const {person} = this.state
-    console.log(person.parentId2)
+    const {parentId1, parentId2, spouseId} = person
 
     return (
       <Page centered={true}>
@@ -33,11 +37,11 @@ class Person extends React.Component {
           <p>Hometown: {person.hometown}</p>
           {person.hasOwnProperty('parentId1')
             && person.hasOwnProperty('parentId2')
-            && <p>Parents: {this._relative(person.parentId1).name} & {this._relative(person.parentId2).name}</p>}
+            && <p>Parents: <Link to={`./${person.parentId1}`}>{this._relative(person.parentId1).name}</Link> & <Link to={`./${person.parentId2}`}>{this._relative(person.parentId2).name}</Link></p>}
           {person.hasOwnProperty('parentId1')
             && !person.hasOwnProperty('parentId2')
             && <p>Parent: {this._relative(person.parentId1).name}</p>}
-          {person.hasOwnProperty('spouseId') && <p>Spouse: {this._relative(person.spouseId).name}</p>}
+          {person.hasOwnProperty('spouseId') && <p>Spouse: <Link to={`./${person.spouseId}`}>{this._relative(person.spouseId).name}</Link></p>}
         </Card>
       </Page>
     )
